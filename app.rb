e1c3eccf3c4ee5138ab1d49d848678339116ccce
Ronnie Taylor <ronnie@ronnietaylor.com>
2012-09-08 16:16:01 -0400

    cleaning out old code

--- app.rb
+++ app.rb
@@ -11,10 +11,4 @@ class App < Sinatra::Application
 	get '/' do
 		haml :index
 	end
-
-  # This is a sample async route. You must enable async sinatra to use this.
-	# aget '/delay/:n' do |n|
-  #   EM.add_timer(n.to_i) { body { "delayed for #{n} seconds" } }
-  # end
-
 end
