ba7247036686fbe0cbbdef0edd3b1bf155568acf
Ronnie Taylor <ronnie@ronnietaylor.com>
2012-10-04 13:46:12 -0400

    major refactor and simplification

--- config.ru
+++ config.ru
@@ -1,17 +1,2 @@
-# config.ru
-
-require 'thin'
-require 'yard'
-require 'sinatra'
-require 'sinatra/contrib'
-require 'sinatra/partial'
-require 'sinatra/flash'
-require 'eventmachine'
-require 'haml'
-require 'sass'
-require 'compass'
-
-# our app file
-require './app'
-
-run App.new
+require './makers'
+run Sinatra::Application
---
+++ makers.rb
@@ -0,0 +1,18 @@
+# Get ready to kick ass.
+require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+end
+
+get '/' do
+  haml :index
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+end
