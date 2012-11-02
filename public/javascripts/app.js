7acf333e809dcf01360aea924d41bb712c1d60db
Ronnie Taylor <ronnie@ronnietaylor.com>
2012-11-02 09:27:31 -0400

    added bg color change for hover

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,5 @@
-//$(document).ready(function() {
-  //$('.speaker-list li').hover(function(){
-    //$(this).toggleClass('s-stripe');
-  //});
-//});
+$(document).ready(function() {
+  $('.speaker-list li a').hover(function(){
+    $(this).parent('div').parent('li').toggleClass('s-stripe');
+  });
+});
